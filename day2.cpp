f5ae8b64ba4216f974d16b22f41a9bd89d6ac71d
Michael Kirsch <michael_kirsch@t-online.de>
2020-12-11 14:05:21 +0100

    add day2_a

--- day2.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by micha on 11.12.2020.
-//
-
-#include "day2.h"
--- day2.h
+++ day2.h
@@ -23,31 +23,54 @@ public:
         *end_of_prefix = div;
     }
 
+    bool ex_a(std::string line){
+        int min;
+        int max;
+        char char_to_find;
+        int end_of_prefix;
+        int count = 0;
+        get_items(line, &min, &max, &char_to_find, &end_of_prefix);
+        for (auto x = end_of_prefix; x < line.size(); x++) {
+            if (line[x] == char_to_find) {
+                count++;
+                if (count > max)
+                    break;
+            }
+        }
+        if (count >= min && count <= max)
+            return true;
+        return false;
+    }
+
+    bool ex_b(std::string line){
+        int min;
+        int max;
+        char char_to_find;
+        int end_of_prefix;
+        get_items(line, &min, &max, &char_to_find, &end_of_prefix);
+        bool first = line[end_of_prefix+1+min]==char_to_find;
+        bool second = line[end_of_prefix+1+max]==char_to_find;
+        if((first+second) ==1)
+            return true;
+        return false;
+    }
+
+
     void run() {
         std::ifstream file("inputs/day2_input.txt");
-        int okay_passwords = 0;
+        int okay_passwords_a = 0;
+        int okay_passwords_b = 0;
         if (file.is_open()) {
             std::string line;
             while (std::getline(file, line)) {
-                int min;
-                int max;
-                char char_to_find;
-                int end_of_prefix;
-                int count = 0;
-                get_items(line, &min, &max, &char_to_find, &end_of_prefix);
-                for (auto x = end_of_prefix; x < line.size(); x++) {
-                    if (line[x] == char_to_find) {
-                        count++;
-                        if (count > max)
-                            break;
-                    }
-                }
-                if (count >= min && count <= max)
-                    okay_passwords++;
+                okay_passwords_a += ex_a(line);
+                okay_passwords_b += ex_b(line);
             }
-            std::cout << "there are " << okay_passwords << " okay passwords!" << std::endl;
             file.close();
         }
+
+        std::cout << "There are " << okay_passwords_a << " okay passwords for Ex A" <<std::endl;
+        std::cout << "There are " << okay_passwords_b << " okay passwords for Ex B" <<std::endl;
     }
 
 };
